100473af9477c0202f978923dd0d66dadb02bd1a
dogukanma <dogukanunal007@gmail.com>
2026-06-22 22:34:56 +0300

    Counting page visits, uses mongodb and nodejs

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('hello world. Docker practice in app.js')
-})
+// docker compose içindeki servis adını (mongodb) kullanıyoruz.
+const mongoUri = process.env.MONGO_URI || 'mongodb://mongodb:27017/testdb'
+
+mongoose.connect(mongoUri)
+  .then(() => console.log('MongoDB bağlantısı kuruldu.'))
+  .catch((err) => console.error('MongoDB bağlantı hatası: ', err));
+
+// storing visitor logs
+const LogSchema = new mongoose.Schema({ timestamp: { type: Date, default: Date.now } });
+const Log = mongoose.model('Log', LogSchema);
+
+app.get('/', async (req, res) => {
+  try {
+    // New log in every login
+    await Log.create({})
+    const totalLogs = await Log.countDocuments();
+
+    res.send(`Hello world, This page is visited ${totalLogs} times.`);
+  } catch (error) {
+    res.status(500).send('Veritabanı işlem hatası.')
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Uygulama http://localhost:${port} adresinde çalışıyor.`)
+  console.log(`Uygulama ${port} portunda çalışıyor.`)
 })
